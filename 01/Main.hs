5ebb3d79424e359ba1653acf49b0b9c347a15c87
tomas6446 <tomas.kozakas@gmail.com>
2024-09-21 14:02:04 +0300

    Add second

--- 01/Main.hs
+++ 01/Main.hs
@@ -1,20 +1,28 @@
 module Main where
+import Test.QuickCheck
 
 main :: IO ()
 main = putStrLn "Module loaded"
 
--- 1.1 Using Basic Boolean Logic
+-- 1
 nAnd :: Bool -> Bool -> Bool
 nAnd a b = not (a && b)
 
--- 1.2 Using Conditional Expressions
 nAnd' :: Bool -> Bool -> Bool
 nAnd' a b = if a && b then False else True
 
--- 1.3 Truth Tables
 nAnd'' :: Bool -> Bool -> Bool
 nAnd'' True  True  = False
 nAnd'' True  False = True
 nAnd'' False True  = True
 nAnd'' False False = True
 
+-- 2
+prop_nAnd_eq_nAnd' :: Bool -> Bool -> Bool
+prop_nAnd_eq_nAnd' a b = nAnd a b == nAnd' a b
+
+prop_nAnd_eq_nAnd'' :: Bool -> Bool -> Bool
+prop_nAnd_eq_nAnd'' a b = nAnd a b == nAnd'' a b
+
+prop_nAnd'_eq_nAnd'' :: Bool -> Bool -> Bool
+prop_nAnd'_eq_nAnd'' a b = nAnd' a b == nAnd'' a b
